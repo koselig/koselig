00bcece2ffa9695f903e59ea5f6231b82da05e7d
Jordan Doyle <jordan@doyle.wf>
2016-10-03 12:49:25 +0100

    Copy post types config over to app

---
+++ config/posttypes.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+    'supplier' => [
+        'public' => true,
+        'capability_type' => 'page',
+        'label' => 'Supplier',
+        'map_meta_cap' => true,
+        'menu_position' => 5,
+        'hierarchical' => false,
+        'rewrite' => false,
+        'query_var' => false,
+        'delete_with_user' => true,
+        'supports' => [
+            'title',
+            'revisions',
+        ]
+    ]
+];
