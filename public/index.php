d604d5017cc17140c8fa0fbdbca9a5788aa48da4
Jordan Doyle <jordan@doyle.wf>
2016-10-12 17:01:41 +0100

    fix global variable wordpress was overriding

--- public/index.php
+++ public/index.php
@@ -49,9 +49,9 @@ $app = require_once __DIR__ . '/../bootstrap/app.php';
 $kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
 
 $response = $kernel->handle(
-    $request = Illuminate\Http\Request::capture()
+    $httpRequest = Illuminate\Http\Request::capture()
 );
 
 $response->send();
 
-$kernel->terminate($request, $response);
+$kernel->terminate($httpRequest, $response);
