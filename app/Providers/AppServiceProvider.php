504c12e2427e796408492586440a3ff030eb99ec
Jordan Doyle <jordan@doyle.wf>
2016-10-17 11:37:40 +0100

    Add an example hook to the service provider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Koselig\Support\Action;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -12,6 +13,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        Action::hook('the_title', function ($title) {
+            return 'Koselig - ' . $title;
+        });
     }
 
     /**
